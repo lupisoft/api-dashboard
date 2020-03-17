2461e3325e615f3fc8e4bdbd3b0444e858655127
Agustin Silio <agustin.silio@hotmail.com>
2020-03-17 20:55:15 -0300

    database config added

--- config/database.go
+++ config/database.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func NewDBConnection() (*sql.DB, error) {
+	user := getEnvWithDefault("DB_USER", "root")
+	password := getEnvWithDefault("DB_PASSWORD", "")
+	host := getEnvWithDefault("DB_HOST", "localhost")
+	port := getEnvWithDefault("DB_PORT", "3306")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/ddd_sample?parseTime=true", user, password, host, port)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func getEnvWithDefault(name, def string) string {
+	env := os.Getenv(name)
+	if len(env) != 0 {
+		return env
+	}
+	return def
+}
--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"github.com/gorilla/mux"
 	"log"
+	"main/config"
 	"main/interfaces"
 	"net/http"
 	"time"
 )
 
 func main()  {
+
+	config.NewDBConnection()
 
 	router := mux.NewRouter()
 	port := ":8000"
