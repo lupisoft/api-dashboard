3df04371f8ee13a3eaffbde51045a54464b5e50f
Agustin Silio <agustin.silio@hotmail.com>
2020-03-17 22:31:50 -0300

    test pong handler

--- interfaces/ping_handler.go
+++ interfaces/ping_handler.go
@@ -1,2 +1,17 @@
 package interfaces
 
+import (
+	"main/interfaces/util"
+	"net/http"
+)
+
+type HandlerPong struct {
+}
+
+func NewHandlerPong() HandlerPong {
+	return HandlerPong{}
+}
+
+func (handler HandlerPong) GetPong(response http.ResponseWriter, request *http.Request) {
+	util.Respond(response, 200, "pong")
+}
--- main.go
+++ main.go
@@ -8,24 +8,18 @@ import (
 	"time"
 )
 
-func main()  {
-
+func main() {
 	router := mux.NewRouter()
 	port := ":8000"
-
 	server := &http.Server{
-		Handler: router,
-		Addr:    port,
+		Handler:      router,
+		Addr:         port,
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
 	handler := interfaces.NewHandlerPong()
-	router.HandleFunc("/{var}/ping", handler.GetPong ).Methods("GET")
+	router.HandleFunc("/ping", handler.GetPong).Methods("GET")
 
 	log.Fatal(server.ListenAndServe())
 }
-
-
-
-
