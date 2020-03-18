54cb81e6fab965b4d0d4f083d7bea1c2d22bfe51
Agustin Silio <agustin.silio@hotmail.com>
2020-03-18 18:39:46 -0300

    clean code

--- config/database.go
+++ config/database.go
@@ -5,11 +5,11 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-func NewDBConnection() (*gorm.DB) {
-
+func NewDBConnection() *gorm.DB {
 	db, err := gorm.Open("mysql", "lupisoft:Agus17533542.@/ddd_sample?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic("Error connection to database !!")
 	}
+
 	return db
 }
