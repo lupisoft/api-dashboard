70cf19540ce8a3a19fb04b156cc2709f5b1ffbd3
Agustin Silio <agustin.silio@hotmail.com>
2020-03-18 21:13:41 -0300

    auth added

--- interfaces/util/auth.go
+++ interfaces/util/auth.go
@@ -1,1 +1,31 @@
 package util
+
+import (
+	"net/http"
+)
+
+func BasicAuth(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("token")
+		if token == "" {
+			Error(w, 400, nil, "invalid or empty token")
+			return
+		}
+
+		authOK, err := checkToken(token)
+		if err != nil {
+			Error(w, 500, err, "connection error with Security Center")
+		}
+
+		if authOK == false {
+			Error(w, 401, nil, "Not authorized")
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	}
+}
+
+func checkToken(token string) (bool, error) {
+	return true, nil
+}
--- main.go
+++ main.go
@@ -23,3 +23,11 @@ func main() {
 
 	log.Fatal(server.ListenAndServe())
 }
+
+func use(h http.HandlerFunc, middleware ...func(http.HandlerFunc) http.HandlerFunc) http.HandlerFunc {
+	for _, m := range middleware {
+		h = m(h)
+	}
+
+	return h
+}
